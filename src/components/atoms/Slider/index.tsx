8389ffcb70330bcb87d467d562f1b1d8e7865841
manasa2107 <82016723+manasa2107@users.noreply.github.com>
2023-02-20 11:56:31 +0530

    Update index.tsx

--- src/components/atoms/Slider/index.tsx
+++ src/components/atoms/Slider/index.tsx
@@ -1,49 +1,40 @@
-import React from "react";
-import { Slider } from "@mui/material";
-
-//import { createTheme,colors, ThemeProvider } from '@mui/material';
-
-// const theme = createTheme ({
-//     slider: {
-//         color: {
-//       trackColor: "yellow",
-//       selectionColor: "red"
-//     }
-// }
-//   });
-
-function valuetext(value: number) {
-    return `${value}`;
-  }
+import React, { useState } from "react";
+import {Box, Slider} from "@mui/material";
 
   const marks = [
     {
-      value: 50,
+      value: 1,
       label: "1BTC = $3,406,069.54",
     },
 ];
-const Slider1 = ()=>{
-  return (
-    // <ThemeProvider theme={theme}>
+const Slider1 =() => {
 
-<Slider
-orientation="vertical"
-size="small"
-defaultValue={50}
-aria-label="amount"
-valueLabelDisplay="auto"
-getAriaValueText={valuetext}
-step={10}
-sx={{height:70,ml:6}}
-marks={marks}
-disabled
-/>
-// </MuiThemeProvider>
+  const [value, setvalue] =useState()
+  const handleChange =(event: any, newValue: React.SetStateAction<any>) => {
+    setvalue(newValue)
+  }
+  
+  return (
+      <><Slider
+      orientation="vertical"
+      size="small"
+      min={0.1}
+      max={2}
+      defaultValue={0.0234510}
+      step={0.005}
+      value={value}
+      onChange={handleChange}
+      aria-label="amount"
+      valueLabelDisplay="auto"
+      // getAriaValueText={valuetext}
+      sx={{ height: 70, ml: 6 }}
+      marks={marks} /> 
+      <Box>value:{value}</Box>
+      </>
 
   );
 }
 
 export default Slider1;
-
 
 
